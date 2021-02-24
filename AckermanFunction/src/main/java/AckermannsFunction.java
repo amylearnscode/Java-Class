64926983a41eee54689cefb773cec59c16761f3e
Amy Gonzales <41878493+amylearnscode@users.noreply.github.com>
2021-02-24 14:53:27 -0800

    Add files via upload

---
+++ AckermanFunction/src/main/java/AckermannsFunction.java
@@ -0,0 +1,27 @@
+
+
+
+public class AckermannsFunction {
+    public static void main(String[] args){
+       
+        System.out.println(ackermann(0, 0));
+        System.out.println(ackermann(0, 1));
+        System.out.println(ackermann(1, 1));
+        System.out.println(ackermann(1, 2));
+        System.out.println(ackermann(1, 3));
+        System.out.println(ackermann(2, 2));
+        System.out.println(ackermann(3, 2));
+        
+      }
+        
+    public static int ackermann(int m, int n){
+       if (m == 0)
+           return n + 1;
+       else if (n == 0)
+           return m - 1;
+       else
+          return ackermann(m - 1, ackermann(m, n - 1));
+       
+   }
+    
+}
